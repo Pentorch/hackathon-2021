da13734e82a27ef0f738f2c716aa2cc947dccaa0
WillyAgustinoEfendi <59289325+Pentorch@users.noreply.github.com>
2021-06-13 03:39:42 +0700

    update title

--- application/views/corona/home.php
+++ application/views/corona/home.php
@@ -4,25 +4,46 @@
 		<div class="row pt-4">
 			<div class="col-sm-4">
 <div class="card text-white bg-warning mb-3">
-  <div class="card-body">
+	<div class="card-body">
+	<div class="row no-gutters align-items-center">
+	<div class="col mr-2">
     <h5 class="card-title">POSITIF</h5>
+	</div>
+	<div class="col-auto">
+	<i class="far fa-3x fa-sad-tear"></i>
+	</div>
+	</div>
     <h5 class="card-text"><?= $value['positif'] ;?></h5>
 	</div>
 	</div>
 	</div>
-	<div class="col-sm-4">
-	<div class="card text-white bg-success mb-3">
+			<div class="col-sm-4">
+<div class="card text-white bg-success mb-3">
 	<div class="card-body">
+	<div class="row no-gutters align-items-center">
+	<div class="col mr-2">
     <h5 class="card-title">SEMBUH</h5>
-    <h5 class="card-text"><?= $value['sembuh'] ;?></h5>
-  </div>
-</div>
-</div>
-<div class="col-sm-4">
+	</div>
+	<div class="col-auto">
+	<i class="far fa-3x fa-smile-beam"></i>
+	</div>
+	</div>
+    <h5 class="card-text"><?= $value['positif'] ;?></h5>
+	</div>
+	</div>
+	</div>
+			<div class="col-sm-4">
 <div class="card text-white bg-danger mb-3">
-  <div class="card-body">
+	<div class="card-body">
+	<div class="row no-gutters align-items-center">
+	<div class="col mr-2">
     <h5 class="card-title">MENINGGAL</h5>
-    <h5 class="card-text"><?= $value['meninggal'] ;?></h5>
+	</div>
+	<div class="col-auto">
+	<i class="far fa-3x fa-sad-cry"></i>
+	</div>
+	</div>
+    <h5 class="card-text"><?= $value['positif'] ;?></h5>
 	<?php }?>
 </div>
 </div>
